3ef20b04d3b0d41e917ef533ffd7b38e25656574
Narvarwalker <87329506+Narvarwalker@users.noreply.github.com>
2021-12-29 09:10:09 -0500

    Add files via upload

---
+++ MessageBoard2.php
@@ -0,0 +1,99 @@
+<!-- Exercise from CH. 6 -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Message Board</title>
+</head>
+<body>
+	<h1>Message Board</h1>
+	<?php
+
+
+		if (isset($_GET["action"])) {
+			if ((file_exists("MessageBoard/messages.txt")) && 
+				(filesize("MessageBoard/messages.txt") != 0)) {
+
+				$MessageArray = file("MessageBoard/messages.txt");
+
+				switch ($_GET["action"]) {
+					case "Delete First": 
+						array_shift($MessageArray);
+						break;
+					case "Delete Last":
+						array_pop($MessageArray);
+						break;
+				}
+
+			}// End of switch statement
+			if (count($MessageArray) > 0) {
+				$NewMessages = implode($MessageArray);
+				$MessageStore = fopen("MessageBoard/messages.txt", "wb");
+				if ($MessageStore === FALSE) {
+					echo "There qas an error updating the message file\n"; 
+				
+				} 
+				else {
+					fwrite($MessageStore, $NewMessages);
+					fclose($MessageStore);
+			
+				} 
+			} // end of count if statement
+			else { 
+				// we are because $MessageArray id empty
+				unlink("MessageBoard/messages.txt");
+			}
+		} // end of file checking statement
+
+	 // end of isset if statement
+
+		if ((!file_exists("MessageBoard/messages.txt")) 
+		 || (filesize("MessageBoard/messages.txt") 
+		 == 0)) {
+		 echo "<p>Sorry, there are no messages
+		 posted.</p>\n";
+
+		} // end of if branch
+		else {
+
+		 	$MessageArray =file("MessageBoard/messages.txt");
+
+		 	echo "<table style=\"background-color:lightgray\"
+		 			border=\"1\" width=\"100%\">\n";
+		 	// end of else branch
+
+			$count = count($MessageArray);
+
+			 // a for loop to work with each individual message in the array
+			for ($i = 0; $i < $count; ++$i) {
+			 		$CurrMsg = explode("~", $MessageArray[$i]);
+
+				echo "<tr>\n";
+				echo "<td width=\"10%\"style=\"text-align:center;
+				 			font-weight:bold\">" , ($i + 1) , "</td>\n";
+				echo "<td width=\"90%\"><span style=\"font-weight:bold\">Subject: </span> " , htmlentities($CurrMsg[0]) ,"<br />\n";
+					echo "<span style=\"font-weight:bold\">Name:
+					 </span> " , htmlentities($CurrMsg[1]) ,
+					 "<br />\n";
+					echo "<span style=\"text-decoration:underline;
+					  font-weight:bold\">Message </span><br />\n"  ,htmlentities($CurrMsg[2]) ,
+					  "</td>\n";
+
+					echo "</tr>\n";
+				} // end of for loop
+				echo "</table>\n";
+			} // end of else branch
+		?>
+		<p><a href="PostMessage.php">Post New Message</a></p>
+
+		<p><a href="MessageBoard.php?action=Delete%20First">
+			Delete First Message</a></p>
+
+		<p><a href="MessageBoard.php?action=Delete%20Last">Delete Last Message</a></p>
+
+		
+
+</body>
+</html>
---
+++ PostMessage.php
@@ -0,0 +1,68 @@
+<!-- Exercise from Chapter 6 -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Post Message</title>
+</head>
+<body>
+	<!-- Add PHP block here -->
+	<?php
+		if(isset($_POST["submit"])) {
+			$Subject = stripslashes($_POST["subject"]);
+			$Name = stripslashes($_POST["name"]);
+			$Message = stripslashes($_POST["message"]);
+
+			// Repalce any'~""' with '-' characters
+			$Subject = str_replace("~", "-", $Subject);
+			$Name = str_replace("~", "-", $Name);
+			$Message = str_replace("~", "-", $Message);
+
+			$MessageRecord = "$Subject~$Name~$Message\n";
+
+			// Let's create a varible to store a new open file's data
+
+			$MessageFile = fopen("MessageBoard/messages.txt", "ab");
+
+			// check that there are no issues with the file before writing the new message to it
+				if ($MessageFile === FALSE) {
+					echo "There was an error saving your message!\n";
+				
+			}
+			else {
+				fwrite($MessageFile, $MessageRecord);
+				fclose($MessageFile);
+					echo "Your message has been saved. \n";
+
+			
+			}
+
+			
+		} // end of main if statement
+	?>
+
+
+
+	
+	<h1>Post New Message</h1>
+	<hr/>
+	<form action="PostMessage.php" method="POST">
+		<span style="font-weight:bold">Subject:</span>
+	 		<input type="text" name="subject" />
+		<span style="font-weight:bold">Name:</span>
+	 		<input type="text" name="name" /><br />
+		<textarea name="message" rows="6"
+	 			cols="80"></textarea><br />
+			<input type="submit" name="submit"
+	 			value="Post Message" />
+			<input type="reset" name="reset"
+	 			value="Reset Form" />
+	</form>
+	<hr/>
+	<p>
+	<a href="MessageBoard.php">View Messages</a>
+	</p>
+
+</body>
+</html>
